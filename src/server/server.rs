b1bf8b04e245c9553bc5ce4a709d47134f783646
ErikHegyi <hegyi.erik.2k8@gmail.com>
2025-07-16 22:58:49 +0200

    Added static_enabled() function

--- src/server/server.rs
+++ src/server/server.rs
@@ -14,11 +14,11 @@ pub struct WebServer {
     /* SERVER DATA */
     ip: String,
     port: u16,
-    
+
     /* STATIC */
     static_url: Option<String>,
     static_dir: Option<PathBuf>,
-    
+
     /* MAP URLs TO FUNCTIONS */
     url_map: Vec<(Regex, Box<dyn Fn(&Request) -> Response>)>
 }
@@ -33,24 +33,24 @@ impl WebServer {
     pub fn set_ip(&mut self, ip: impl ToString) {
         self.ip = ip.to_string()
     }
-    
+
     pub fn set_port(&mut self, port: u16) {
         self.port = port
     }
-    
+
     pub fn set_static_url(&mut self, url: impl ToString) {
         self.static_url = Some(url.to_string())
     }
-    
+
     pub fn set_static_dir(&mut self, dir: PathBuf) {
         self.static_dir = Some(dir)
     }
-    
-    pub fn disable_static(&mut self) { 
+
+    pub fn disable_static(&mut self) {
         self.static_url = None;
         self.static_dir = None;
     }
-    
+
     pub fn add_path(&mut self, pattern: &str, function: impl Fn(&Request) -> Response + 'static) {
         let re: Regex = Regex::new(&format!("^{pattern}$")).unwrap();
         self.url_map.push(
@@ -58,58 +58,63 @@ impl WebServer {
         )
     }
     
+    /* ACCESS PROPERTIES */
+    pub fn static_enabled(&self) -> bool {
+        self.static_dir != None
+    }
+
     /* HANDLE REQUESTS */
-    fn serve_static(path: &PathBuf) -> Response {
+    fn serve_static(path: &str) -> Response {
         todo!()
     }
-    
-    
+
+
     fn handle(&self, request: &Request) -> Response {
         // Get the URL of the request
         let url: &str = request.url.as_str();
-        
+
         if let Some(static_url) = &self.static_url {
             if url.starts_with(static_url) {
                 // Test if static files should be served
-                if let Some(dir) = &self.static_dir {
-                    return Self::serve_static(dir);
+                if self.static_enabled() {
+                    return Self::serve_static(url);
                 } else {
                     panic!("Static files are disabled, but a static file is expected: {url}")
-                } 
+                }
             }
         }
-        
+
         // Match the URL to the given patterns
         for (pattern, function) in self.url_map.iter() {
             if pattern.is_match(url) {
                 return function(request);
             }
         }
-        
+
         panic!("Unable to match url ({url}) to any of the patterns.")
     }
-    
+
     /* START SERVER */
     pub fn start(&self) -> Result<()> {
         // Start the TCP listener
         let url: String = format!("{ip}:{port}", ip=self.ip, port=self.port);
         let listener = TcpListener::bind(url)?;
-        
+
         // Listen to incoming requests
         for stream in listener.incoming() {
             let stream = stream?;
-            
+
             // Interpret the request
             let mut request: Request = Request::from(stream);
             println!("{request}");
-            
+
             // Handle the request
             let response: Response = self.handle(&request);
-            
+
             // Write the response
             request.respond(response)?;
         }
-        
+
         Ok(())
     }
 }
