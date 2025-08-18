6e42a85c35d5844a4c3fcb618b814a0e4c29a3b9
ErikHegyi <hegyi.erik.2k8@gmail.com>
2025-08-18 18:34:59 +0200

    Deleted row.rs

--- src/sql/mod.rs
+++ src/sql/mod.rs
@@ -1,12 +1,10 @@
 mod database;
 mod table;
 mod sql_types;
-mod row;
 mod column;
 
 pub use database::Database;
 pub use table::{Table, SQLTable};
-pub use row::{Row, RowValue};
 pub use column::Column;
 pub use sql_types::SQLType;
 pub use sql_types::SQLValue;
